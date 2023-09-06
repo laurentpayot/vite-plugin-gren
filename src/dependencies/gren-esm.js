53885222ba408d7c9a10fc6d704f3f897981df9a
Laurent Payot <laurent.payot@publiberty.com>
2023-09-06 22:46:39 +0200

    gren-esm:  regex fixes for Gren compiler output

--- src/dependencies/gren-esm.js
+++ src/dependencies/gren-esm.js
@@ -3,13 +3,13 @@ const toESModule = js => {
         // /^\s*_Platform_export\(([^]*)\);\n?}\(this\)\);/m
         /^\s*_Platform_export\((.*)\);\n?}/m
     )[1];
-    console.log("*** grenExports =", grenExports)
     return js
         .replace(/\(function\s*\(scope\)\s*\{$/m, "// -- $&")
         .replace(/['"]use strict['"];$/m, "// -- $&")
         .replace(/function _Platform_export([^]*?)\}\n/g, "/*\n$&\n*/")
         .replace(/function _Platform_mergeExports([^]*?)\}\n\s*}/g, "/*\n$&\n*/")
-        .replace(/^\s*_Platform_export\(([^]*)\);\n?}\(this\)\);/m, "/*\n$&\n*/")
+        // .replace(/^\s*_Platform_export\(([^]*)\);\n?}\(this\)\);/m, "/*\n$&\n*/")
+        .replace(/^\s*_Platform_export\(.*;$/m, "/*\n$&\n*/")
         .concat(`
 export const Gren = ${grenExports};
   `);
